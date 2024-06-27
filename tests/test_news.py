f4a1e363139a86553ec32ff7cbc014cc7c695127
artyuan <arthuryuan23@gmail.com>
2024-06-26 23:36:33 -0300

    Fix: removing test_newsapi for now.

--- tests/test_news.py
+++ tests/test_news.py
@@ -8,11 +8,11 @@ load_dotenv()
 class TestNews(unittest.TestCase):
     def setUp(self):
         self.ticker = 'Ibovespa'
-    def test_newsapi(self):
-        api_key = os.getenv('newsapi_key')
-        get_news = NewsFromTicker(self.ticker)
-        df_news = get_news.newsapi(api_key)
-        self.assertFalse(df_news.empty)
+    # def test_newsapi(self):
+    #     api_key = os.getenv('newsapi_key')
+    #     get_news = NewsFromTicker(self.ticker)
+    #     df_news = get_news.newsapi(api_key)
+    #     self.assertFalse(df_news.empty)
     def test_google_news(self):
         get_news = NewsFromTicker(self.ticker)
         df_news = get_news.googlenews()
